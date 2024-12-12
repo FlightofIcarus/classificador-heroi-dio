fcd2facb8660bc84e6710acc8b5f5ee017f673a0
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-12-12 00:13:38 -0300

    Refatoracao com lacos de repeticao

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 const prompt = require('prompt-sync')();
 
+const heroLevel = [{
+    nivel:'Ferro',
+    pontuacaoMaxima: 1000,
+    pontuacaoMinima: 0},
+    {
+        nivel:'Bronze',
+        pontuacaoMaxima: 2000,
+        pontuacaoMinima: 1001},
+    {
+        nivel:'Prata',
+        pontuacaoMaxima: 5000,
+        pontuacaoMinima: 2001},
+        {
+        nivel:'Ouro',
+        pontuacaoMaxima: 7000,
+        pontuacaoMinima: 5001},
+        {
+        nivel:'Platina',
+        pontuacaoMaxima: 8000,
+        pontuacaoMinima: 7001},
+        {
+        nivel:'Ascendente',
+        pontuacaoMaxima: 9000,
+        pontuacaoMinima: 8001},
+        {
+        nivel:'Imortal',
+        pontuacaoMaxima: 10000,
+        pontuacaoMinima: 9001},
+        {
+        nivel:'Radiante',
+        pontuacaoMaxima: 10000,
+        pontuacaoMinima: 10001
+        }
+]
+
+
 const myHeroName = prompt('Digite seu nome.');
 const myHeroXp = parseInt(prompt('Digite seu valor de experiência.'));
-let heroLevel;
 
-    if (myHeroXp < 1000) {
-        heroLevel = 'Ferro';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Bronze';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Prata';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Ouro';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Platina';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Ascendente';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Imortal';
-    }else if (myHeroXp >= 1001 && myHeroXp <= 2000) {
-        heroLevel = 'Radiante';
-    }
-console.log(`O herói ${myHeroName} tem ${myHeroXp} pontos de experiência e está no nível ${heroLevel}`);
+heroLevel.forEach(element => {
+    if(myHeroXp >= element.pontuacaoMinima && myHeroXp <= element.pontuacaoMaxima){
+        console.log(`O herói ${myHeroName} tem ${myHeroXp} pontos de experiência e está no nível ${element.nivel}.`);
+          }
+})
